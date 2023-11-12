ff4ee5c852c94b7477efe947ed6c407fc2e3b6dd
Allef Ramos <77581479+Allef-O-Ramos@users.noreply.github.com>
2023-11-12 18:51:29 +0000

    GET, CREATE, POST, UPDATE & DELETE in character/id

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+// Registrar um MidWare
+// Indica que tds as requisições podem receber Body em JSON
+// Express aplica JSON.parse para conteúdo recebido
 
-app.get("/oi", function (req, res) {
-  res.send("Olá mundo");
+app.use(express.json())
+
+
+app.get("/", function (req, res) {
+  res.send("Gravity Falls Characters");
 });
 
 const lista = ["Dipper", "Mabbel", "Stan", "Soos"];
-app.get("/Gravity_Falls", function (req, res) {
-  res.send(lista[1])
+app.get("/character", function (req, res) {
+  res.send(lista)
 });
+
+//READ by ID /character
+app.get("/character/:id", function (req, res) {
+  const id = req.params.id - 1
+  const item = lista[id]
+  res.send(item)
+});
+
+// Create - [POST] /item
+app.post("/character", function(req, res){
+  const item = req.body.nome
+  lista.push(item)
+  res.send("Adicionado com sucesso")
+});
+
+// Update - [PUT] /item/id
+app.put("/character/:id", function(req, res){
+  const id = req.params.id - 1
+  const novoItem = req.body.nome
+  lista[id] = novoItem
+  res.send("Item atualizado com sucesso!")
+})
+
+// Delete - [DELETE] /item/id
+app.delete("/character/:id", function(req, res){
+  const id = req.params.id - 1
+  delete lista[id]
+  res.send("Itam removido com sucesso") 
+})
 
 app.listen(3000);
